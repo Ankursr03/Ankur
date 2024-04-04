83bf5ad6338ba1f430cd29de1dfe5074f5fcc8b3
Ankursr03 <ankursr03@gmail.com>
2024-04-04 23:56:05 +0530

    homepage

--- src/login page/Student.jsx
+++ src/login page/Student.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 
 function Admin() {
   const [AdId, setAdId] = useState("");
   const [pass, setPass] = useState("");
   const [selectedUserType, setSelectedUserType] = useState("Student");
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(() => {
+    const images = [
+      "bh.jpg",
+      "loginbackground.jpg",
+      "1.jpg",
+      "2.jpg",
+      "3.jpg",
+      
+      
+      // Add more image URLs as needed
+    ];
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setBackgroundImage(images[randomIndex]);
+  }, []);
 
   function handleIdChange(event) {
     setAdId(event.target.value);
@@ -24,7 +39,10 @@ function Admin() {
   }
 
   return (
-    <div className="background-template">
+    <div
+      className="background-template"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <div className="admin-container">
         <div className="user-type-dropdown">
           <select value={selectedUserType} onChange={handleUserTypeChange}>
@@ -39,9 +57,22 @@ function Admin() {
           <h1>LOGIN</h1>
           <div className="inputs">
             <label htmlFor="username">USERNAME:</label>
-            <input type="text" value={AdId} onChange={handleIdChange} placeholder="Enter Id" id="username"/><br />
+            <input
+              type="text"
+              value={AdId}
+              onChange={handleIdChange}
+              placeholder="Enter Id"
+              id="username"
+            />
+            <br />
             <label htmlFor="password">PASSWORD:</label>
-            <input type="password" value={pass} onChange={handlePassChange} placeholder="••••••••" id="password"/>
+            <input
+              type="password"
+              value={pass}
+              onChange={handlePassChange}
+              placeholder="••••••••"
+              id="password"
+            />
           </div>
           <button onClick={handleLogin}>LOGIN</button>
         </div>
